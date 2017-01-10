4daf7f51b3fcbcd4f118a8b94a6ab223d22b9298
HarshaGUDev <harshagu.gubbi@gmail.com>
2017-01-10 11:04:35 +0530

    Double UI2

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,31 @@
+angular.module('hello', []).controller('home',
+
+function($http) {
+	
+	var self = this;
+	
+	console.log('Loading');
+
+	$http.get('user').then(function(response) {
+		var data = response.data;
+		if (data.name) {
+			self.authenticated = true;
+			self.user = data.name
+			$http.get('/resource/').then(function(response) {
+				self.greeting = response.data;
+			})
+		} else {
+			self.authenticated = false;
+		}
+	}, function() {
+		self.authenticated = false;
+	});
+	
+//	self.logout = function() {
+//		$http.post('/logout', {}).finally(function() {
+//			self.authenticated = false;
+//			 window.history.go(-2);
+//		});
+//	}
+
+});
